3987edbbbc64b9fe1f6617cf42aab669cdff6ad1
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-13 08:34:42 +0000

    Done.

---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,8 @@
+class Add_grade_and_birthdate_to_students < ActiveRecord::Migration[5.1]
+
+  def change
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
+  end
+
+end
